1cc84270da2e708bef179381be8546ee325dfdc4
hadsome-Droid <xkumbhax@gmail.com>
2023-09-30 11:04:14 +0300

    create Container component

---
+++ src/components/Container.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1245px;
+  min-height: 100%;
+  width: 100%;
+  margin: 0 auto;
+  outline: 1px solid red;
+`
--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import {Icon} from "../../../components/icon/Icon";
 import Styled from "../../../assets/images/different/styled-components.webp"
+import styled from "styled-components";
+import {Container} from "../../../components/Container";
 
 export const Skills = () => {
     return (
-        <div>
-            <Icon iconId={'html'}/>
-            <Icon iconId={'css'}/>
-            <Icon iconId={'react'}/>
-            <Icon iconId={'js'}/>
-            <Icon iconId={'figma'}/>
-            <Icon iconId={'github'}/>
-            <Icon iconId={'linkedin'}/>
-            <Icon iconId={'email'}/>
-            <Icon iconId={'telegram'}/>
-            <Icon iconId={'whatsapp'}/>
-            <Icon iconId={'typescript'}/>
-            <img src={Styled} alt=""/>
-        </div>
+        <StyledSkills>
+            <Container>
+
+                <Icon iconId={'html'}/>
+                <Icon iconId={'css'}/>
+                <Icon iconId={'react'}/>
+                <Icon iconId={'js'}/>
+                <Icon iconId={'figma'}/>
+                <Icon iconId={'github'}/>
+                <Icon iconId={'linkedin'}/>
+                <Icon iconId={'email'}/>
+                <Icon iconId={'telegram'}/>
+                <Icon iconId={'whatsapp'}/>
+                <Icon iconId={'typescript'}/>
+                <img src={Styled} alt=""/>
+            </Container>
+
+        </StyledSkills>
     );
 };
+
+const StyledSkills = styled.section`
+  background-color: #4F47C71B;
+`
